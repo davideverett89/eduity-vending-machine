0137491bcee267f0f60f50c0942fd53c90b8869d
David Everett <davideverett1989@gmail.com>
2020-11-04 11:11:29 -0600

    Basic authentication in place.

---
+++ src/components/Auth.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Auth mx-auto col-6 my-5 border border-dark rounded p-5 bg-light">
+    <h1 class="display-4">
+      Please Login
+    </h1>
+    <p class="lead my-5">
+      In order to better serve our customers, we need some basic user info. 
+      By clicking below, we will store your display name and email address. 
+      These will not be shared with anyone else.
+    </p>
+    <button
+      class="btn btn-primary login-btn"
+      @click.prevent="login"
+    >
+      Login
+    </button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export default {
+    name: "Auth",
+    methods:{
+        login() {
+            const provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider);
+        }
+    }
+}
+</script>
---
+++ src/components/VendingMachine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="VendingMachine">
+    <h1>Vending Machine</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
