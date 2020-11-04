383f6f5d5a12324074b98d80d08301e0963b8c8f
David Everett <davideverett1989@gmail.com>
2020-11-04 11:27:05 -0600

    Added login logic to check if a customer has logged in before or not.

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -21,12 +21,38 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import customerData from '../helpers/data/customerData';
+import authData from '../helpers/data/authData';
+
 export default {
     name: "Auth",
     methods:{
         login() {
             const provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(provider);
+            firebase.auth().signInWithPopup(provider)
+              .then(() => {
+                this.customerCheck()
+              })
+              .catch((err) => console.error('Unable to login:', err));
+        },
+        customerCheck() {
+          customerData.getCustomerByEmail(authData.getEmail())
+            .then((response) => {
+              if (response.length === 0) {
+                this.handleCreateCustomer();
+              }
+            })
+            .catch((err) => console.error('There was an issue checking for customer information at login:', err));
+        },
+        handleCreateCustomer() {
+          const newCustomer = {
+            display_name: authData.getDisplayName(),
+            email: authData.getEmail(),
+            uid: authData.getUid(),
+          }
+          customerData.postCustomer(newCustomer)
+            .then(() => {})
+            .catch((err) => console.error('There was an issue posting new customer information:', err));
         }
     }
 }
